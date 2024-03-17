5439c4cd155c023d0e24ae3eff80d1ca737ada22
mrBEASTBOY <hlinh0409@gmail.com>
2024-03-16 20:48:05 -0500

    Fix bug in AttendanceSystemApplication. Add package com.ea.group.four.attendancesystem to scanBasePackages

--- src/main/java/com/ea/group/four/attendancesystem/AttendanceSystemApplication.java
+++ src/main/java/com/ea/group/four/attendancesystem/AttendanceSystemApplication.java
@@ -3,7 +3,7 @@ package com.ea.group.four.attendancesystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "edu.miu.common, edu.miu.cs.cs544")
+@SpringBootApplication(scanBasePackages = "edu.miu.common, com.ea.group.four.attendancesystem")
 public class AttendanceSystemApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/com/ea/group/four/attendancesystem/service/RoleService.java
@@ -0,0 +1,8 @@
+package com.ea.group.four.attendancesystem.service;
+
+import com.ea.group.four.attendancesystem.domain.Role;
+import com.ea.group.four.attendancesystem.service.response.response.RoleResponse;
+import edu.miu.common.service.BaseReadWriteService;
+
+public interface RoleService extends BaseReadWriteService<RoleResponse, Role, Long> {
+}
