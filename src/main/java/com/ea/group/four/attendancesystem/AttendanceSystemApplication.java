4b6d20ac3753a964b5580896189bb5dc8206a16a
samnang-an <samnang.an7@gmail.com>
2024-03-17 08:19:15 -0500

    refactor: scanbase package structure

--- src/main/java/com/ea/group/four/attendancesystem/AttendanceSystemApplication.java
+++ src/main/java/com/ea/group/four/attendancesystem/AttendanceSystemApplication.java
@@ -3,7 +3,7 @@ package com.ea.group.four.attendancesystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.ea.group.four.attendancesystem", "edu.miu.common"})
+@SpringBootApplication(scanBasePackages = {"edu.miu.common", "com.ea.group.four.attendancesystem"})
 public class AttendanceSystemApplication {
 
   public static void main(String[] args) {
