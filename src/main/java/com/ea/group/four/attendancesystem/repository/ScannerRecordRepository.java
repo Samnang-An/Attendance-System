a7e50851cc3534cb97c44be9b2b25c3640314f0b
mrBEASTBOY <hlinh0409@gmail.com>
2024-03-17 15:59:29 -0500

    CRUD for Scanner Second time

--- src/main/java/com/ea/group/four/attendancesystem/repository/ScannerRecordRepository.java
+++ src/main/java/com/ea/group/four/attendancesystem/repository/ScannerRecordRepository.java
@@ -3,6 +3,8 @@ package com.ea.group.four.attendancesystem.repository;
 import com.ea.group.four.attendancesystem.domain.ScanRecord;
 import edu.miu.common.repository.BaseRepository;
 
-public interface ScannerRecordRepository extends BaseRepository<ScanRecord, Long> {
+import java.util.List;
 
+public interface ScannerRecordRepository extends BaseRepository<ScanRecord, Long> {
+    public List<ScanRecord> findByScanner(Scanner scanner);
 }
