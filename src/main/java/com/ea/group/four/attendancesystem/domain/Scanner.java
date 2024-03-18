6d48d3cfe9c8fc406951e8c1fb41134acef61b0b
mrBEASTBOY <hlinh0409@gmail.com>
2024-03-17 20:16:23 -0500

    Fix import in Account Type and application.yml

--- src/main/java/com/ea/group/four/attendancesystem/domain/Scanner.java
+++ src/main/java/com/ea/group/four/attendancesystem/domain/Scanner.java
@@ -1,8 +1,15 @@
 package com.ea.group.four.attendancesystem.domain;
 
-import jakarta.persistence.*;
 
 import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
