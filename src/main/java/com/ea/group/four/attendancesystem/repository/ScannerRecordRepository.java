32e5ab83f7bda0231aa3b96a37622bedd5528e72
Nahom-Asmelash <nahomasmelash0997@gmail.com>
2024-03-19 18:30:30 -0500

    unit Test for MemberService

--- src/main/java/com/ea/group/four/attendancesystem/repository/ScannerRecordRepository.java
+++ src/main/java/com/ea/group/four/attendancesystem/repository/ScannerRecordRepository.java
@@ -12,9 +12,10 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
+@Repository
 public interface ScannerRecordRepository extends BaseRepository<ScanRecord, Long> {
 
   List<ScanRecord> findByScanner(Scanner scanner);
